f7bbdff63a7cd5d22a47bec9ae10726b9b163ec7
Ib Kshash <ibrahim.kshash@gmail.com>
2012-01-31 19:18:35 -0500

    some improvements

--- plugins/twitter.rb
+++ plugins/twitter.rb
@@ -11,44 +11,42 @@ class Twitter
 		return nil if minutes < 0
 
 		case minutes
-		when 0..4            then '5 minutes'
-		when 5..14           then '15 minutes'
-		when 15..29          then '30 minutes'
-		when 30..59          then '30 minutes'
+		when 0..1      then "Just now"
+		when 2..59     then "#{minutes.to_s} minutes ago"
 		when 60..1439        
 			words = (minutes/60)
 			if words > 1
-				"#{words.to_s} hours"
+				"#{words.to_s} hours ago"
 			else
-				"#{words.to_s} hour"
+				"#{words.to_s} hour ago"
 			end
 		when 1440..11519     
 			words = (minutes/1440)
 			if words > 1
-				"#{words.to_s} days"
+				"#{words.to_s} days ago"
 			else
-				"#{words.to_s} day"
+				"#{words.to_s} day ago"
 			end
 		when 11520..43199    
 			words = (minutes/11520)
 			if words > 1
-				"#{words.to_s} weeks"
+				"#{words.to_s} weeks ago"
 			else
-				"#{words.to_s} week"
+				"#{words.to_s} week ago"
 			end
 		when 43200..525599   
 			words = (minutes/43200)
 			if words > 1
-				"#{words.to_s} months"
+				"#{words.to_s} months ago"
 			else
-				"#{words.to_s} month"
+				"#{words.to_s} month ago"
 			end
 		else                      
 			words = (minutes/525600)
 			if words > 1
-				"#{words.to_s} years"
+				"#{words.to_s} years ago"
 			else
-				"#{words.to_s} year"
+				"#{words.to_s} year ago"
 			end
 		end
 	end
@@ -67,7 +65,7 @@ class Twitter
 			time        = Time.parse(posted)
 			time        = minutes_in_words(time)
 
-			m.reply "12Twitter #{name} (@#{screenname}): #{tweettext} | Posted #{time} ago"
+			m.reply "12Twitter #{name} (@#{screenname}): #{tweettext} | Posted #{time}"
 		rescue Timeout::Error
 			m.reply "12Twitter Timeout Error. Maybe twitter is down?"
 		rescue
